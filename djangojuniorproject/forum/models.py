7c8fa7fd45a3bff9e1b3e5b40677d925e33e885a
davidalsh <software.dev.davidalsh@gmail.com>
2021-09-10 16:31:31 +0200

    Models.py in forum changed

--- djangojuniorproject/forum/models.py
+++ djangojuniorproject/forum/models.py
@@ -17,21 +17,3 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created_at']
-
-
-# class UserProfile(models.Model):
-#
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-#     about = models.CharField(max_length=300, blank=True)
-#     # avatar = ResizedImageField(size=[230, 230], crop=['middle', 'center'], quality=99, upload_to='profileavatar/%Y/%m/%d', null=True, blank=True)
-#
-#
-# class Comment(models.Model):
-#
-#     text = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#
-#     owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='comment')
-#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
-
-
